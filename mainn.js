027024445d21e8e959652cd54c40d7f3d137f8c5
Yasmin <125517021+alunayasmin@users.noreply.github.com>
2023-10-11 16:10:48 -0300

    Add files via upload

---
+++ mainn.js
@@ -0,0 +1,10 @@
+function createCanvas()
+{
+    createCanvas(300, 300);
+}
+
+circle(30, 30, 20);
+describe('A white circle with black outline in the middle of a gray canvas.');
+
+rect(30, 20, 55, 55);
+describe('A white rectangle with a black outline on a gray canvas.');
